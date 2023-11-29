610eaf81e9a943003cee84ab9d59f85b607962b3
mylian21 <mpenam02@gmail.com>
2023-11-28 22:23:17 -0300

    Programación Aplicada

--- TP-POO/TP_Algoritmos/Ejercicio_5.1.py
+++ TP-POO/TP_Algoritmos/Ejercicio_5.1.py
@@ -3,17 +3,17 @@ número entero. Si el número es impar, restarle 3 y sumarlo. Si el número
 da negativo, sumar 1."""
 
 def sumar_digitos(numero):
-    if numero % 2 == 0:
+    if numero % 2 == 0 and numero > 0:
 # En caso de que sea un numero par le suma los pares
         suma_pares = sum(int(digito) for digito in str(numero) if int(digito) % 2 == 0)
         return suma_pares
 # Si el resultado es impar, restar 3 y sumarlo.    
-    elif numero % 2 == 1:
+    elif numero % 2 == 1 and numero > 0:
         total=(numero-3)+numero
         return total
 # Si el resultado es negativo, sumar 1
     elif numero < 0:
-        numneg=numero+1
+        numneg = numero + 1
         return numneg
     
 resultado1 = sumar_digitos(2468)
@@ -22,5 +22,5 @@ print(f"Resultado 1: {resultado1}")  #Imprime: 20
 resultado2 = sumar_digitos(7)
 print(f"Resultado 2: {resultado2}")  #Imprime: 11
 
-resultado3= sumar_digitos(-3)
+resultado3= sumar_digitos(-4)
 print(f"Resultado 3: {resultado3}") 
